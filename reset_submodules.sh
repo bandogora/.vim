424895c68f39aaa77946f7beb7f69caa8c54a2ea
jforgue <26842354+bandogora@users.noreply.github.com>
2021-11-22 13:58:22 -0500

    Update setup instructions and script

--- reset_submodules.sh
+++ reset_submodules.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-git submodule init
-git submodule update
-git submodule foreach 'git fetch origin; git checkout $(git rev-parse --abbrev-ref HEAD); git reset --hard origin/$(git rev-parse --abbrev-ref HEAD); git submodule update --recursive; git clean -dfx'
+git -C $HOME/.vim submodule init
+git -C $HOME/.vim submodule update
+git -C $HOME/.vim submodule foreach 'git fetch origin; git checkout $(git rev-parse --abbrev-ref HEAD); git reset --hard origin/$(git rev-parse --abbrev-ref HEAD); git submodule update --recursive; git clean -dfx'
 
-sh ./helptags.sh
+[ -f $HOME/.vim/helptags.sh ] && $HOME/.vim/helptags.sh
---
+++ setup.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+function die() {
+  echo "${@}"
+  exit 1
+}
+
+# Add .local to existing Vim and Bash config files in the home directory
+# or add .old if .local files already exist
+for i in $HOME/.vimrc $HOME/.gvimrc $HOME/.bashrc $HOME/.bash_aliases; do
+  if [[ ( -e $i ) ]]; then
+    if [ -e ${i}.local ]; then
+      echo "${i}.local already exists"
+      echo "Renaming ${i} to ${i}.old"
+      mv ${i} ${i}.old || die "Could not move ${i} to ${i}.old"
+    else
+      echo "Renaming ${i} to ${i}.local"
+      mv ${i} ${i}.local || die "Could not move ${i} to ${i}.local"
+    fi
+  fi
+done
+
+# Symlink .vim config files to $HOME dir
+ln -s $HOME/.vim/vimrc $HOME/.vimrc || die "Could not symlink .vimrc file"
+ln -s $HOME/.vim/gvimrc $HOME/.gvimrc || die "Could not symlink .gvimrc file"
+ln -s $HOME/.vim/bashrc $HOME/.bashrc || die "Could not symlink .bashrc file"
+ln -s $HOME/.vim/bash_aliases $HOME/.bash_aliases || die "Could not symlink .bash_aliases file"
+
+# Call reset_modules.sh
+[ -f $HOME/.vim/reset_submodules.sh ] && $HOME/.vim/reset_submodules.sh
